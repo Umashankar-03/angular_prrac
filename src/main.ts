1ffac24c642a6c29186bc9fe4193d54e348b9a1b
Umashankar Tiwari <preettiwari2018@gmail.com>
2025-07-23 11:38:24 +0530

    2nd commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { LoginCompComponent } from './login-comp/login-comp.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(LoginCompComponent , appConfig)
+  .catch((err) => console.error(err));
+
+  bootstrapApplication(AppComponent , appConfig)
   .catch((err) => console.error(err));
